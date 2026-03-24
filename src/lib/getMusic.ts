60664f0b93531ef6225a31a6a685b866b4162ede
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-03-24 23:42:43 +0000

    feat: getMusic function added as mood-based music mappping utility

---
+++ src/lib/getMusic.ts
@@ -0,0 +1,58 @@
+
+
+export function getMusic(mood: string) {
+
+    // pick relevant block of case code
+    // depending on mood
+    switch (mood.toLowerCase()) {
+
+        case "energetic":
+            return {
+                song: "Another Day Of Sun",
+                artist: "La La Land Cast",
+                embedUrl: "https://open.spotify.com/track/5kRBzRZmZTXVg8okC7SJFZ?si=f29370c78acd4e9c",
+            };
+
+        case "calm":
+            return {
+                song: "Blue",
+                artist: "Hoody",
+                embedUrl: "https://open.spotify.com/track/75jyO0tWZ6wSFdhk4y8Spr?si=719d414fc4804936",
+            };
+        
+        case "reflective":
+            return {
+                song: "La petite fille de la mer - Remastered",
+                artist: "Vangelis",
+                embedUrl: "https://open.spotify.com/track/2lB8swV3T9yiXyU6OnZ3tY?si=7265992cf5654cbb",
+            };
+
+        case "cozy":
+            return {
+                song: "Nadhiyae Nadhiyae",
+                artist: "Unni Menon",
+                embedUrl: "https://open.spotify.com/track/6oAMvvfm6kQhADxsfTC9Ah?si=6a64b3857b5e4ba0",
+            };
+
+        case "intense":
+            return {
+                song: "Blizzard",
+                artist: "Thomas Bergersen, Two Steps From Hell",
+                embedUrl: "https://open.spotify.com/track/5Bo4UGQit7a6hivXOjoM2t?si=ab10bd9a528a4baf",
+            };
+
+        case "peaceful":
+            return {
+                song: "The Well-Tempered Clavier, Book 1: Prelude No.1 in C Major, BWV 846/1",
+                artist: "Johann Sebastian Bach, Maurizio Pollini",
+                embedUrl: "https://open.spotify.com/track/2w2FzZKSwudBZjOsCei9Qz?si=f51ec7ce395b468d",
+            };
+
+        case "neutral":
+            return {
+                song: "Idea 15",
+                artist: "Gibran Alcocer",
+                embedUrl: "https://open.spotify.com/track/7MJ8g0egEkhvGu44IamrDE?si=cb8bab95f1b14857",
+            };
+    }
+}
