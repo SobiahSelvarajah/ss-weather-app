ac2d97d08badceba509231b31d472d765307427c
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-06-12 12:06:33 +0100

    components: Header html styled and responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/layout/Header";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-slate-900 text-white min-h-screen">
       <Header/>
     </main>
   );
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,12 @@
 
 export default function Header() {
     return(
-        <section>
-            <div>
-                <h1>
+        <section className="px-7 py-10 md:p-15 lg:p-20">
+            <div className="mx-auto max-w-6xl lg:text-center">
+                <h1 className="text-4xl font-light tracking-tight md:text-5xl lg:text-6xl">
                     Weather Forecast
                 </h1>
-                <p>
+                <p className="mt-3 max-w-sm text-sm text-slate-400 font-medium md:text-base lg:mx-auto">
                     Weather, activities and music tailored to your day.
                 </p>
             </div>
