5344365b5546f768f7cc1d0bfd247a88e71e3e77
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-08-31 12:47:39 +0100

    style: activity card alignment and spacing adjusted

--- src/components/activities/ActivitySuggestions.tsx
+++ src/components/activities/ActivitySuggestions.tsx
@@ -20,15 +20,15 @@ export default function ActivitySuggestions ({
 
     
     return (
-        <section className="h-full w-full rounded-3xl border border-white/10 bg-white/5 p-6 backdrop-blur-xl">
-            <h3 className="text-lg font-medium pb-3">
+        <section className="h-full w-full rounded-3xl border border-white/10 bg-white/5 p-6 backdrop-blur-xl md:flex md: flex-col">
+            <h3 className="pb-3 text-lg font-medium">
                 Suggested Activities
             </h3>
-            <ul className="mt-4 space-y-2">
+            <ul className="mt-4 space-y-2 md:flex md:flex-1 md:flex-col md:justify-center md:space-y-0 md:gap-3">
                 {suggestions.map((activity) => (
                     <li 
                         key={activity.text}
-                        className="flex items-center rounded-xl border border-white/10 bg-white/5 p-3"
+                        className="flex items-center rounded-xl border border-white/10 bg-white/5 p-3 md:p-4 lg:p-5"
                     >
                         <span className="mr-2 text-lg">
                             {activity.icon}
